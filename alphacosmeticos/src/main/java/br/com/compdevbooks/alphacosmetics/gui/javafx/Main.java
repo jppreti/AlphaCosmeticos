8eab93530b693ea816fb6aa80d192ff644cb8e21
jppreti <jppreti@gmail.com>
2016-06-01 20:02:19 -0400

    Feitas correções de dimensões na interface FrmCliente.

---
+++ alphacosmeticos/src/main/java/br/com/compdevbooks/alphacosmetics/gui/javafx/Main.java
@@ -0,0 +1,38 @@
+package br.com.compdevbooks.alphacosmetics.gui.javafx;
+
+import java.io.IOException;
+
+import br.com.compdevbooks.alphacosmetics.gui.javafx.controller.FrmCliente;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+        BorderPane frmCliente = null;
+        
+        try{
+            frmCliente = FXMLLoader.load(FrmCliente.class.getClassLoader().getResource("gui//FrmCliente.fxml"));
+        }catch (IOException ioe){
+            ioe.printStackTrace();
+        }
+
+        primaryStage.setTitle("Formulário de Manutenção de Clientes");
+        primaryStage.setWidth(800);
+        primaryStage.setHeight(600);
+        
+        Scene scene = new Scene(frmCliente);
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+}
--- alphacosmeticos/src/main/java/br/com/compdevbooks/alphacosmetics/gui/javafx/controller/FrmCliente.java
+++ alphacosmeticos/src/main/java/br/com/compdevbooks/alphacosmetics/gui/javafx/controller/FrmCliente.java
@@ -1,5 +1,124 @@
 package br.com.compdevbooks.alphacosmetics.gui.javafx.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.event.ActionEvent;
+
 public class FrmCliente {
+
+    @FXML
+    private Button btnConfirmar;
+
+    @FXML
+    private TextField txtPesqNome;
+
+    @FXML
+    private Label lblPesqNome;
+
+    @FXML
+    private HBox hbxEditButtons;
+
+    @FXML
+    private Tab tabClientes;
+
+    @FXML
+    private Label lblCodigoValue;
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtTelefone;
+
+    @FXML
+    private Label lblEmail;
+
+    @FXML
+    private VBox vbxBottom;
+
+    @FXML
+    private TableView<?> tblClientes;
+
+    @FXML
+    private Label lblStatus;
+
+    @FXML
+    private GridPane grdEdicao;
+
+    @FXML
+    private Button btnProcurar;
+
+    @FXML
+    private Button btnAlterar;
+
+    @FXML
+    private Button btnCancelar;
+
+    @FXML
+    private TabPane tbpCliente;
+
+    @FXML
+    private Label lblTelefone;
+
+    @FXML
+    private Tab tabEdicao;
+
+    @FXML
+    private Button btnExcluir;
+
+    @FXML
+    private Label lblCodigo;
+
+    @FXML
+    private Label lblNome;
+
+    @FXML
+    private Button btnNovo;
+
+    @FXML
+    void btnNovo_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnConfirmar_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnAlterar_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCancelar_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnExcluir_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtPesqNome_onKeyPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnProcurar_onAction(ActionEvent event) {
+
+    }
 
 }
