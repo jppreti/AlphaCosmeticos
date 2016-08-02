5cc6f35a248a4d051267da8ac8509aed483941e4
Lenovo <Lenovo@192.168.1.6>
2016-08-02 06:14:58 -0400

    CRUD para o Cliente, Fornecedor e Vendedor

--- src/main/java/br/com/compdevbooks/alphacosmetics/gui/javafx/controller/cadastro/FrmCliente.java
+++ src/main/java/br/com/compdevbooks/alphacosmetics/gui/javafx/controller/cadastro/FrmCliente.java
@@ -1,23 +1,22 @@
 package br.com.compdevbooks.alphacosmetics.gui.javafx.controller.cadastro;
 
-import br.com.compdevbooks.alphacosmetics.business.Vendedor;
+import br.com.compdevbooks.alphacosmetics.business.Cliente;
 import br.com.compdevbooks.alphacosmetics.dao.DAOFactory;
-import br.com.compdevbooks.alphacosmetics.entity.pessoa.VendedorEntity;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import br.com.compdevbooks.alphacosmetics.entity.pessoa.ClienteEntity;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableColumn.CellDataFeatures;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.StackPane;
 import javafx.util.Callback;
 
 public class FrmCliente {
@@ -29,13 +28,22 @@ public class FrmCliente {
     private TextField txtPesqNome;
 
     @FXML
-    private TableView<VendedorEntity> tableViewCliente;
+    private Button botao_limpar;
+
+    @FXML
+    private TableView<ClienteEntity> tableViewCliente;
+
+    @FXML
+    private TextField txtClienteInscricao;
 
     @FXML
     private Tab tabCliente;
 
     @FXML
-    private Label nomeFantasia1;
+    private Button botao_salvar;
+
+    @FXML
+    private StackPane stkDialog;
 
     @FXML
     private TextField txtClienteBairro;
@@ -47,7 +55,7 @@ public class FrmCliente {
     private TextField txtClienteFantasia;
 
     @FXML
-    private Label nomeFantasia;
+    private TextField txtClienteEstado;
 
     @FXML
     private Button btnProcurar;
@@ -74,120 +82,64 @@ public class FrmCliente {
     private TextField txtClienteNumero;
 
     @FXML
-    private Button botao_limpar;
-
-    @FXML
-    private TextField txtClienteInscricao;
-
-    @FXML
-    private Button botao_salvar;
-
-    @FXML
-    private StackPane stkDialog;
-
-    @FXML
-    private TextField txtClienteEstado;
-
-    @FXML
-    private Button botao_cancelar;
-
-    @FXML
-    private Button buttonCadastrar;
-
-    @FXML
-    private Label nomeFantasia11;
-
-    @FXML
-    private Label razaoSocial;
-
-    @FXML
     private TextField txtClienteRazaoSocial;
     
-    Vendedor vendedor = new Vendedor(DAOFactory.getDAOFactory().getVendedorDAO());
-    private boolean novo = true;
+    Cliente cliente = new Cliente(DAOFactory.getDAOFactory().getClienteDAO());
     
     //Inicializa os elementos da TableViewCliente
     @FXML
     void initialize() {
-		TableColumn<VendedorEntity, String> tbcNome = new TableColumn<VendedorEntity, String>("Nome");
-		tbcNome.setCellValueFactory(new Callback<CellDataFeatures<VendedorEntity, String>, ObservableValue<String>>() {
-			public ObservableValue<String> call(CellDataFeatures<VendedorEntity, String> c) {
-				return new ReadOnlyObjectWrapper<String>(c.getValue().getNome());
+                //Adciona a Coluna Nome
+		TableColumn<ClienteEntity, String> tbcNome = new TableColumn<ClienteEntity, String>("Nome");
+		tbcNome.setCellValueFactory(new Callback<CellDataFeatures<ClienteEntity, String>, ObservableValue<String>>() {
+			public ObservableValue<String> call(CellDataFeatures<ClienteEntity, String> c) {
+				return new ReadOnlyObjectWrapper<String>(c.getValue().getFantasia());
 			}
 		});
+                
                 tableViewCliente.getColumns().add(tbcNome);
                 atualiza_Cliente();
+    } 
+    
+    void atualiza_Cliente(){
+        ObservableList<ClienteEntity> produtos = FXCollections.observableArrayList(cliente.getByNome(txtPesqNome.getText()));
+        tableViewCliente.setItems(produtos);
     }
-    
-    @FXML
-    void btnCadastrar(ActionEvent event) {
-    
-    }
-    
-    //Método para Alterar
+
     @FXML
     void btnAlterar(ActionEvent event) {
-        VendedorEntity ent = tableViewCliente.getSelectionModel().getSelectedItem();
+        ClienteEntity ent = tableViewCliente.getSelectionModel().getSelectedItem();
         //Deleta a informação anterior e adiciona a nova informação no TextField
 		if (ent != null) {
-                    vendedor.delete(tableViewCliente.getSelectionModel().getSelectedItem());
-                    txtClienteFantasia.setText(ent.getNome());
-	            txtClienteCnpj.setText(ent.getRG());
-		    txtClienteInscricao.setText(ent.getCPF());
+                    cliente.delete(tableViewCliente.getSelectionModel().getSelectedItem());
+                    txtClienteFantasia.setText(ent.getInscricao());
 		}
     }
-    
-    //Método para Excluir
+
     @FXML
     void btnExcluir(ActionEvent event) {
         if (tableViewCliente.getSelectionModel().getSelectedIndex() >= 0) {
-            vendedor.delete(tableViewCliente.getSelectionModel().getSelectedItem());
+            cliente.delete(tableViewCliente.getSelectionModel().getSelectedItem());
             atualiza_Cliente();
         }
     }
 
     @FXML
-    void botao_cancelar(ActionEvent event) {
-
-    }
-
-    //Método para Cadastrar    
-    @FXML
     void button_salvar(ActionEvent event) {
-        VendedorEntity ent;
-        ent = new VendedorEntity();
-	ent.setNome(txtClienteFantasia.getText());
-	vendedor.save(ent);
+        ClienteEntity ent = new ClienteEntity();
+	ent.setInscricao(txtClienteInscricao.getText());
+	cliente.save(ent);
         atualiza_Cliente();
     }
 
-    //Método para Limpar os campos
     @FXML
-    void limpar_dados(){
-        txtClienteRazaoSocial.setText("");
+    void limpar_dados(ActionEvent event) {
         txtClienteFantasia.setText("");
-        txtClienteCnpj.setText("");
-        txtClienteInscricao.setText("");
-        
-        txtClienteLogradouro.setText("");  
-        txtClienteNumero.setText("");  
-        txtClienteCep.setText("");
-        txtClienteBairro.setText("");
-        
-        txtClienteMunicipio.setText("");
-        txtClienteEstado.setText("");
-    }	
-    
-    //Pesquisa Por Nome
+    }
+
     @FXML
     void botao_pesquisa(ActionEvent event) {
         atualiza_Cliente();
     }
-    
-    void atualiza_Cliente(){
-        final ObservableList<VendedorEntity> produtos = FXCollections.observableArrayList(vendedor.getByNome(txtPesqNome.getText()));
-        tableViewCliente.setItems(produtos);
-    }
-    
 
 }
--- src/main/java/br/com/compdevbooks/alphacosmetics/gui/javafx/controller/cadastro/FrmFornecedor.java
+++ src/main/java/br/com/compdevbooks/alphacosmetics/gui/javafx/controller/cadastro/FrmFornecedor.java
@@ -1,58 +1,64 @@
 package br.com.compdevbooks.alphacosmetics.gui.javafx.controller.cadastro;
 
+import br.com.compdevbooks.alphacosmetics.business.Fornecedor;
+import br.com.compdevbooks.alphacosmetics.dao.DAOFactory;
+import br.com.compdevbooks.alphacosmetics.entity.pessoa.FornecedorEntity;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellDataFeatures;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.util.Callback;
 
 public class FrmFornecedor {
-
-    @FXML
-    private MenuItem mniEditar;
-
-    @FXML
-    private Label nomeFantasia11111;
-
-    @FXML
-    private TextField txtNome11;
 
     @FXML
     private TextField txtPesqNome;
 
     @FXML
+    private TextField txtInscricao;
+
+    @FXML
     private HBox hbxEditButtons1;
+
+    @FXML
+    private HBox hbxEditButtons;
 
     @FXML
     private Tab tabCliente;
 
     @FXML
-    private TextField txtNome;
+    private TableView<FornecedorEntity> tableViewFornecedor;
+
+    @FXML
+    private TextField txtEmail;
 
     @FXML
     private VBox vbxBottom;
 
     @FXML
-    private Label nomeFantasia1;
+    private TextField txtNumero;
 
     @FXML
-    private TextField txtNome2;
+    private Label lblStatus;
 
     @FXML
-    private TextField txtNome1;
+    private StackPane stkDialog;
 
     @FXML
-    private Label nomeFantasia1111;
-
-    @FXML
-    private Label nomeFantasia;
+    private TextField txtCnpj;
 
     @FXML
     private Button btnProcurar;
@@ -67,73 +73,99 @@ public class FrmFornecedor {
     private Tab tabEditar;
 
     @FXML
-    private ContextMenu ctxMenu;
+    private TextField txtRazaoSocial;
 
     @FXML
     private Button buttonEditar;
 
     @FXML
-    private TextField txtNome21;
+    private TextField txtCep;
 
     @FXML
-    private HBox hbxEditButtons;
+    private TextField txtBanco;
 
     @FXML
-    private TextField txtNome111;
+    private TextField txtLogradouro;
 
     @FXML
-    private TextField txtNome112;
+    private TextField txtFantasia;
 
     @FXML
-    private TextField txtNome211;
-
+    private TextField txtBairro;
+    
+    Fornecedor fornecedor = new Fornecedor(DAOFactory.getDAOFactory().getFornecedorDAO());
+    
+    //Inicializa os elementos da TableViewCliente
     @FXML
-    private Label lblStatus;
-
-    @FXML
-    private StackPane stkDialog;
-
-    @FXML
-    private TextField txtNome1121;
-
-    @FXML
-    private TextField txtNome2111;
-
-    @FXML
-    private Button buttonCadastrar;
-
-    @FXML
-    private Label nomeFantasia11;
-
-    @FXML
-    private MenuItem mniExcluir;
-
-    @FXML
-    private Label razaoSocial;
-
-    @FXML
-    void mniEditar_onAction(ActionEvent event) {
-
+    void initialize() {
+                //Adciona a Coluna Nome
+		TableColumn<FornecedorEntity, String> tbcNome = new TableColumn<FornecedorEntity, String>("Nome Fantasia");
+		tbcNome.setCellValueFactory(new Callback<CellDataFeatures<FornecedorEntity, String>, ObservableValue<String>>() {
+			public ObservableValue<String> call(CellDataFeatures<FornecedorEntity, String> c) {
+				return new ReadOnlyObjectWrapper<String>(c.getValue().getFantasia());
+			}
+		});
+                
+                TableColumn<FornecedorEntity, String> tbcCnpj = new TableColumn<FornecedorEntity, String>("Cnpj");
+		tbcCnpj.setCellValueFactory(new Callback<CellDataFeatures<FornecedorEntity, String>, ObservableValue<String>>() {
+			public ObservableValue<String> call(CellDataFeatures<FornecedorEntity, String> c) {
+				return new ReadOnlyObjectWrapper<String>(c.getValue().getCNPJ());
+			}
+		});
+                
+                tableViewFornecedor.getColumns().add(tbcNome);
+                tableViewFornecedor.getColumns().add(tbcCnpj);
+                atualiza_Fornecedor();
+                
     }
-
-    @FXML
-    void mniExcluir_onAction(ActionEvent event) {
-
+    
+    void atualiza_Fornecedor(){
+        final ObservableList<FornecedorEntity> produtos = FXCollections.observableArrayList(fornecedor.getByNome(txtPesqNome.getText()));
+        tableViewFornecedor.setItems(produtos);
     }
-
-    @FXML
-    void botao_cadastrar_fornecedor(ActionEvent event) {
-
-    }
+    
+    
 
     @FXML
     void botao_editar_fornecedor(ActionEvent event) {
-
+        if (tableViewFornecedor.getSelectionModel().getSelectedIndex() >= 0) {
+            FornecedorEntity ent = tableViewFornecedor.getSelectionModel().getSelectedItem();
+        //Deleta a informação anterior e adiciona a nova informação no TextField
+		if (ent != null) {
+                    fornecedor.delete(tableViewFornecedor.getSelectionModel().getSelectedItem());
+                    txtFantasia.setText(ent.getFantasia());
+                    txtCnpj.setText(ent.getCNPJ());
+		}
+        }
     }
 
     @FXML
     void botao_remover_fornecedor(ActionEvent event) {
+         if (tableViewFornecedor.getSelectionModel().getSelectedIndex() >= 0) {
+            fornecedor.delete(tableViewFornecedor.getSelectionModel().getSelectedItem());
+            atualiza_Fornecedor();
+        }
+    }
 
+    @FXML
+    void Salvar_Dados(ActionEvent event) {
+        FornecedorEntity ent;
+        ent = new FornecedorEntity();
+	ent.setFantasia(txtFantasia.getText());
+        ent.setCNPJ(txtCnpj.getText());
+	fornecedor.save(ent);
+        atualiza_Fornecedor();
+    }
+
+    @FXML
+    void Limpar_Dados(ActionEvent event) {
+        txtFantasia.setText("");
+        txtCnpj.setText("");
+    }
+
+    @FXML
+    void Pesquisa(ActionEvent event) {
+        atualiza_Fornecedor();
     }
 
 }
